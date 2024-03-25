6287737b9018cc0b1386d760bd13e4671ff49b34
Turon Kuzibaev <kuzibaevturonbek@gmail.com>
2024-03-26 02:16:27 +0500

    config changes

--- src/graphql/core/config.py
+++ src/graphql/core/config.py
@@ -14,12 +14,12 @@ class Settings(BaseSettings):
     PROJECT_TITLE: str = "Fast Api GraphQL Strawberry"
     PROJECT_VERSION: str = "0.0.1"
     HOST_HTTP: str = os.getenv("HOST_HTTP", "http://")
-    HOST_URL: str = os.getenv("HOST_URL", "127.0.0.1")
-    HOST_PORT: int = int(os.getenv("HOST_PORT", 8000))
+    HOST_URL: str = os.getenv("HOST_URL")
+    HOST_PORT: int = int(os.getenv("HOST_PORT"))
     BASE_URL: str = HOST_HTTP + HOST_URL + ":" + str(HOST_PORT)
     POSTGRES_USER: str = os.getenv("POSTGRES_USER")
     POSTGRES_PASSWORD: str = os.getenv("POSTGRES_PASSWORD")
-    POSTGRES_SERVER: str = os.getenv("POSTGRES_SERVER", "127.0.0.1")
+    POSTGRES_SERVER: str = os.getenv("POSTGRES_SERVER")
     POSTGRES_PORT: int = int(os.getenv("POSTGRES_PORT", 5432))
     POSTGRES_DB: str = os.getenv("POSTGRES_DB")
     DATABASE_URL = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_SERVER}:{POSTGRES_PORT}/{POSTGRES_DB}"
