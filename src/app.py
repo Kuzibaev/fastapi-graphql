4d341a2b1b8f4c8265e1f2149f1ef813450a2c74
Turon Kuzibaev <kuzibaevturonbek@gmail.com>
2024-03-26 11:24:59 +0500

    middlewares

--- src/app.py
+++ src/app.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 import strawberry
 from strawberry.fastapi import GraphQLRouter
 from strawberry.schema.config import StrawberryConfig
@@ -10,6 +11,17 @@ schema = strawberry.Schema(query=Query, mutation=Mutation, config=StrawberryConf
 
 def create_app():
     app = FastAPI()
+    create_create_middlewares(app)
     graphql_app = GraphQLRouter(schema)
     app.include_router(graphql_app, prefix="/graphql")
     return app
+
+
+def create_create_middlewares(app):
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=["*"],  # Update this to allow specific origins
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
